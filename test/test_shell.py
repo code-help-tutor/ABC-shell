50e889e5dadb1ced610d5188bc94a75b2d4f55c2
Code Help Tutor <1823890830@qq.com>
2024-04-10 06:08:51 +0000

    WeChat cstutorcs ABC shell

---
+++ test/test_shell.py
@@ -0,0 +1,19 @@
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+import unittest
+
+from shell import eval
+from collections import deque
+
+
+class TestShell(unittest.TestCase):
+    def test_shell(self):
+        out = deque()
+        eval("echo foo", out)
+        self.assertEqual(out.popleft(), "foo\n")
+        self.assertEqual(len(out), 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
